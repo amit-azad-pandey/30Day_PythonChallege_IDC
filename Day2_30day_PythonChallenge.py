3549914f60edbdef22599861736c90b152956f48
amit pandey <35841970+amit-azad-pandey@users.noreply.github.com>
2025-05-29 21:08:27 +0530

    Add files via upload

---
+++ Day2_30day_PythonChallenge.py
@@ -0,0 +1,17 @@
+#🔍 Topics Covered: Integers & floats, Strings & booleans, Type conversion with int(), float(), and str()
+
+# Challenge
+# Calculate the area of a rectangle using user-input length and width
+
+# inputs
+len = float (input("Enter the length of the rectangle: "))
+wid = float (input("Enter the width of the rectangle: "))
+
+#input value being cm
+
+# formula of rectangle
+# Area = length * width
+area = len * wid
+
+# result
+print(f"Area of the rectangle is {area} square cm")
